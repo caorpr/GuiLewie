ebe71d9f7d9d094ad192ad2fa67e5bd227ce3dd1
caorpr <cand3952@740s-pl-110.local>
2015-11-09 13:48:25 -0700

    Added listener for the button!

--- src/gui/view/GuiPanel.java
+++ src/gui/view/GuiPanel.java
@@ -1,10 +1,8 @@
 package gui.view;
 
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.SpringLayout;
-
+import javax.swing.*; // You can do this so that everything in "javax.swing." is imported! Can mess up HUGE projects apparently, 
+import java.awt.event.ActionListener;     //  but Mr. H says it is okay for any of the projects that we do.
+import java.awt.event.ActionEvent;
 import gui.controller.GuiController;
 
 public class GuiPanel extends JPanel
@@ -19,12 +17,16 @@ public class GuiPanel extends JPanel
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		firstButton = new JButton("Do not click the button");
+
 		firstTextField = new JTextField("Type Words Here");
-		
+				
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
+	/**
+	 * Loads content into the panel.
+	 */
 	
 	private void setupPanel()
 	{
@@ -33,14 +35,31 @@ public class GuiPanel extends JPanel
 		this.add(firstTextField);
 	}
 	
+	/**
+	 * Helper method to hold this crap GUI layout code.
+	 */
+	
 	private void setupLayout()
 	{
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 171, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 170, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, firstButton, 361, SpringLayout.WEST, this);
 		
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 0, SpringLayout.WEST, firstButton);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstTextField, -4, SpringLayout.NORTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, 290, SpringLayout.WEST, this);
+
 	}
 	
 	private void setupListeners()
 	{
-		
+		firstButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				
+			}
+		});
 	}
 	
 	
