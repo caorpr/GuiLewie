eef76aaf702cf42c2ac2ac7817f435be012164aa
caorpr <cand3952@740s-pl-110.local>
2015-11-09 12:51:27 -0700

    made a base panel and guipanel

--- src/gui/view/GuiFrame.java
+++ src/gui/view/GuiFrame.java
@@ -6,15 +6,25 @@ import gui.controller.GuiController;
 /**
  * 
  * @author cand3952
- * @version 0.1 November 9, 2015.
+ * @version 0.2 November 9, 2015.
  */
 
 public class GuiFrame extends JFrame
 {
 	private GuiController baseController;
+	private GuiPanel basePanel;
 	
 	public GuiFrame(GuiController baseController)
 	{
-		this.baseController = baseController;
+		this.baseController = baseController; //Assign parameter to data member.
+		basePanel = new GuiPanel(baseController); // instance of GuiPanel with reference to controller.
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setResizable(false);   //They can't resize it, automatically true.
+		this.setSize(400, 400);		//Sets the size.
+		this.setVisible(true);      //This will be you last line of code in the setupFrame method.
 	}
 }
